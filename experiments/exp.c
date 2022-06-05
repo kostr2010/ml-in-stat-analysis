1cf7060b7e886f337614d02b62564ecba3b76fd6
Konstantin Nazarov <kostr2010@gmail.com>
2022-06-05 04:41:38 +0300

    i give up

--- experiments/exp.c
+++ experiments/exp.c
@@ -1,13 +1,24 @@
+// #include "exp.h"
 #include <stdlib.h>
 #include <string.h>
 
+static const int FIVE = 5;
+
+int* data = NULL;
+
+void a()
+{
+    data = (int*)calloc(100, sizeof(int));
+    if (FIVE != 5) {
+        free(data);
+    }
+}
+
 int main()
 {
-    int* a = malloc(10 * sizeof(int));
+    a();
 
-    free(a);
-
-    a[5] = 10;
+    free(data);
 
     return 0;
 }
---
+++ experiments/exp.h
@@ -0,0 +1,2 @@
+
+static const int FIVE = 5;
